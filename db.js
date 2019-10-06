bf71aa150e7cbbd9e995d93a44df173b66d8176f
Glitch (hello-express) <none>
2019-10-06 17:05:33 +0000

    📫🚏 Checkpoint ./db.js:1334717/3218

--- db.js
+++ db.js
@@ -1,56 +1,76 @@
 const Realm = require("realm");
 
 // Define your models and their properties
-const CarSchema = {
-  name: "Car",
+const TeamSchema = {
+  name: "Team",
   properties: {
-    make: "string",
-    model: "string",
-    miles: { type: "int", default: 0 }
+    name: "string",
+    league: "League"
   }
 };
 
-const PersonSchema = {
-  name: "Person",
+const PlayerSchema = {
+  name: "Player",
   properties: {
     name: "string",
     birthday: "date",
-    cars: "Car[]",
-    picture: "data?" // optional property
+    teams: "Team[]"
   }
 };
 
-Realm.open({ schema: [CarSchema, PersonSchema] })
+const LeagueSchema = {
+  name: "League",
+  properties: {
+    name: "string"
+  }
+};
+
+const DB = realm => ({
+  create: (schema, data) => {
+    realm.write(() => realm.create(schema, data))
+  },
+
+})
+
+Realm.open({ schema: [TeamSchema, PlayerSchema, LeagueSchema] })
   .then(realm => {
-    // Create Realm objects and write to local storage
-    realm.write(() => {
-      const myCar = realm.create("Car", {
-        make: "Honda",
-        model: "Civic",
-        miles: 1000
-      });
-      myCar.miles += 20; // Update a property value
-    });
+    const db = DB(realm)
+    
+    const leagues = realm.objects("League")
+    db.create('League', {
+      ''
+    })
+    console.log('Leagues:', leagues)
 
-    // Query Realm for all cars with a high mileage
-    const cars = realm.objects("Car").filtered("miles > 1000");
+    //     // Create Realm objects and write to local storage
+    //     realm.write(() => {
+    //       const myCar = realm.create("Car", {
+    //         make: "Honda",
+    //         model: "Civic",
+    //         miles: 1000
+    //       });
+    //       myCar.miles += 20; // Update a property value
+    //     });
 
-    // Will return a Results object with our 1 car
-    cars.length; // => 1
+    //     // Query Realm for all cars with a high mileage
+    //     const cars = realm.objects("Car").filtered("miles > 1000");
 
-    console.log(cars);
+    //     // Will return a Results object with our 1 car
+    //     cars.length; // => 1
 
-    // Add another car
-    realm.write(() => {
-      const myCar = realm.create("Car", {
-        make: "Ford",
-        model: "Focus",
-        miles: 2000
-      });
-    });
+    //     console.log(cars);
 
-    // Query results are updated in realtime
-    cars.length; // => 2
+    //     // Add another car
+    //     realm.write(() => {
+    //       const myCar = realm.create("Car", {
+    //         make: "Ford",
+    //         model: "Focus",
+    //         miles: 2000
+    //       });
+    //     });
+
+    //     // Query results are updated in realtime
+    //     cars.length; // => 2
   })
   .catch(error => {
     console.log(error);
