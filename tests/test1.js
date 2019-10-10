d8da79d8f8ed024dfc884e8d709c6a44206c062a
Glitch (hello-express) <none>
2019-10-10 20:04:19 +0000

    🎹💋 Checkpoint ./tests/test1.js:1334717/1470 ./db.js:1334717/70

--- tests/test1.js
+++ tests/test1.js
@@ -1,15 +1,45 @@
-const fs = require('fs');
-const test = require('tape');
+const fs = require('fs')
+const test = require('tape')
+const Realm = require("realm")
+const hyperid = require('hyperid')
+const uuid = hyperid(true)
+const schemas = require('../schemas')
 
 // Start with a fresh DB
+const files = [
+'default.realm.lock',
+'default.realm.note',
+'default.realm',
+'default.realm.management'
+]
+
+files.forEach(file => fs.existsSync(file) ? fs.unlinkSync(file) : null)
 
 test('timing test', function (t) {
-    t.plan(2);
+//     t.plan(2)
 
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
+//     t.equal(typeof Date.now, 'function')
+//     var start = Date.now()
 
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
-});
+//     setTimeout(function () {
+//         t.equal(Date.now() - start, 100)
+//     }, 100)
+  
+  Realm.open({
+    path: 'test.realm',
+    schema: [schemas.TeamSchema, schemas.PlayerSchema, schemas.LeagueSchema]
+  })
+  .then(realm => {
+    const leagues = realm.objects("League")
+    const data = {
+      id: uuid(),
+      name: 'Serie A',
+      season: '2019/2020'
+    }
+    realm.create('League', data)
+    t.ok(leagues[0])
+    t.equal(leagues[0].name, 'Serie A')
+  })
+  
+  
+})
