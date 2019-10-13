928c8a55725816c83cabe4282fbefea3d22e85ea
Glitch (hello-express) <none>
2019-10-13 16:33:23 +0000

    📉💽 Checkpoint ./schemas.js:1334717/656 ./.glitch-assets:1334717/405

--- schemas.js
+++ schemas.js
@@ -1,52 +1,76 @@
 module.exports.LeagueSchema = {
-  name: "League",
+  name: 'League',
   primaryKey: 'id',
   properties: {
     id: 'string',
-    name: "string",
+    name: 'string',
     // country: 'Country'
   }
 }
 
 module.exports.SeasonSchema = {
-  name: "Season",
-  primaryKey: 'id',
+  name: 'Season',
+  primaryKey: 'key',
   properties: {
     id: 'string',
     key: 'string',
-    name: "string"
+    name: 'string'
   }
 }
 
 module.exports.CompetitionSchema = {
-  name: "Competition",
+  name: 'Competition',
+  primaryKey: 'key',
+  properties: {
+    id: 'string',
+    key: 'string',
+    name: 'string',
+    league: 'League',
+    season: 'Season',
+    start: 'date',
+    end: 'date',
+    teams: 'Team[]'
+  }
+}
+
+module.exports.GroupSchema = {
+  name: 'Group',
   primaryKey: 'id',
   properties: {
     id: 'string',
-    name: "string",
-    league: "League",
-    season: "Season",
-    start: "date",
-    end: "date"
+    name: 'string',
+    competition: 'Competition',
+    num: 'number'
+  }
+}
+
+module.exports.RoundSchema = {
+  name: 'Round',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    name: 'string',
+    competition: 'Competition',
+    num: 'number'
   }
 }
 
 module.exports.TeamSchema = {
-  name: "Team",
+  name: 'Team',
   primaryKey: 'id',
   properties: {
     id: 'string',
-    name: "string",
-    league: "League"
+    name: 'string',
+    league: 'League'
   }
 }
 
 module.exports.PlayerSchema = {
-  name: "Player",
+  name: 'Player',
   primaryKey: 'id',
   properties: {
     id: 'string',
-    name: "string",
-    teams: "Team[]"
+    name: 'string',
+    teams: 'Team[]'
   }
 }
