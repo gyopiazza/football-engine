21ff08a53939ba0e4e658c615bd4fa52a4a70194
Glitch (realmdb) <none>
2019-10-26 18:37:12 +0000

    🖇🖐 Checkpoint ./api.js:1334717/432

--- api.js
+++ api.js
@@ -49,11 +49,11 @@ const standingsSorter = (a, b) => {
 const calculateCup = ({ competition, rounds, matches, groups }) => {
   console.log(matches)
   return groups.reduce((standings, group) => {
+    console.log(standings[group.name])
     standings[group.name] = matches
-        // .filter(match => match.group && match.group.name === group.name)
+        .filter(match => match.group && match.group.name === group.name)
         .reduce(standingsReducer, [])
         .sort(standingsSorter)
-    // console.log(standings[group.name])
     return standings
   }, {})
 }
