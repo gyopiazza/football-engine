6e7ebdd731389e8cbc4f8c1211d0218a5703b5d5
Glitch (football-realmdb) <none>
2019-10-28 17:14:32 +0000

    💫🚵 Checkpoint ./tests/data/seed.js:1334717/257

--- tests/data/seed.js
+++ tests/data/seed.js
@@ -1,8 +1,18 @@
+const hyperid = require('hyperid')
+const uuid = hyperid(true)
 const serieAClubs = require('./seriea-clubs-2017-18.json')
 const serieAMatches = require('./seriea-2017-18.json')
 
 const seed = (realm, done) => {
   realm.write(() => {
+    
+    serieAClubs.clubs.forEach(team => {
+      const palermo = realm.create('Team', {
+        id: uuid(),
+        name: team.name
+      })
+    })
+    
     done()
   })
 }
