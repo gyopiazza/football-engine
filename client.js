20b49e5d5ac1ddf1d023522f2c46f41f2747be4d
Glitch (football-realmdb) <none>
2019-11-04 11:41:57 +0000

    🕌🌦 Checkpoint ./client.js:1334717/76

--- client.js
+++ client.js
@@ -6,6 +6,8 @@ const Log = (() => {
   return message => [effectFn, { message }]
 })()
 
+const effectFn = (dispatch, opts) => console.log(opts.message)
+
 const test = (state, payload) => {
   // return [state * 2, Log(payload)]
   return [state * 2, [test2, payload]]
@@ -22,7 +24,7 @@ app({
       h("h1", {}, state),
       h("button", { onClick: state => state - 1 }, "-"),
       h("button", { onClick: state => state + 1 }, "+"),
-      h("button", { onClick: [test, {test: true}] }, "load")
+      h("button", { onClick: [effectFn, {test: true}] }, "load")
     ]),
   node: document.getElementById("app")
 })
