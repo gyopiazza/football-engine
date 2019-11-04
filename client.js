c449d60120edd354f66b86665b38177dd0c39bc2
Glitch (football-realmdb) <none>
2019-11-04 11:06:00 +0000

    💓🌓 Checkpoint ./package.json:1334717/2763 ./client.js:1334717/235 ./server.js:1334717/105

--- client.js
+++ client.js
@@ -1,4 +1,14 @@
 import { h, app } from "hyperapp"
+import { Http } from "hyperapp-fx"
+
+const GetQuote = () => [
+  "...",
+  Http({
+    url:
+      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    action: (_, [{ content }]) => content
+  })
+]
 
 app({
   init: 0,
