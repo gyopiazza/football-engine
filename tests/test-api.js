5a354a2b4cd0c36cfe855fd9a5b3c0f4352b203f
Glitch (football-realmdb) <none>
2020-11-29 09:50:51 +0000

    ⛺️💐 Checkpoint ./tests/test-api.js:1334717/475 ./tests/test-various.js:1334717/5

--- tests/test-api.js
+++ tests/test-api.js
@@ -11,19 +11,20 @@ function log() {
 //////////////////////////////////////////////////////
 
 
-const mockSchedule = (teamsNum, options) => {
+const mockSchedule = (teamsNum = 10, options = {}) => {
   const teams = []
   for (let i = 1; i <= teamsNum; i++) {
     teams.push({ id: i, name: 'Team ' + i })
   }
   
-  const schedule = api.generateSchedule(teams, { twolegs: true, shuffle: true })
+  const schedule = api.generateSchedule(teams, options)
   // Count home and away matches for each team
   const homeAwayCount = api.countHomeAwayMatches(schedule)
   // Get the first result as reference
   const homeAwayReferenceValues = homeAwayCount[Object.keys(homeAwayCount)[0]]
   
   return {
+    teams,
     schedule,
     homeAwayCount,
     homeAwayReferenceValues
@@ -35,43 +36,46 @@ const mockSchedule = (teamsNum, options) => {
 
 
 test('API Tests', function (t) {
-
-  const teams = [
-    { id: 1, name: 'Team 1' },
-    { id: 2, name: 'Team 2' },
-    { id: 3, name: 'Team 3' },
-    { id: 4, name: 'Team 4' },
-    { id: 5, name: 'Team 5' },
-    // { id: 6, name: 'Team 6' },
-    // { id: 7, name: 'Team 7' },
-  ]
-  const compensate = teams.length % 2 ? 0 : 2
   
-  const schedule = api.generateSchedule(teams, { twolegs: true, shuffle: true })
+  const mock = mockSchedule(11, { twolegs: true, shuffle: true })
+  
+  // const teams = [
+  //   { id: 1, name: 'Team 1' },
+  //   { id: 2, name: 'Team 2' },
+  //   { id: 3, name: 'Team 3' },
+  //   { id: 4, name: 'Team 4' },
+  //   { id: 5, name: 'Team 5' },
+  //   // { id: 6, name: 'Team 6' },
+  //   // { id: 7, name: 'Team 7' },
+  // ]
+  const compensate = mock.teams.length % 2 ? 0 : 2
+  
+  // const schedule = api.generateSchedule(teams, { twolegs: true, shuffle: true })
   // Count home and away matches for each team
-  const homeAwayCount = api.countHomeAwayMatches(schedule)
+  // const homeAwayCount = api.countHomeAwayMatches(schedule)
   // Get the first result as reference
-  const homeAwayReferenceValues = homeAwayCount[Object.keys(homeAwayCount)[0]]
+  // const homeAwayReferenceValues = homeAwayCount[Object.keys(homeAwayCount)[0]]
   
-  console.log('teams.length', teams.length)
-  console.log('rest', teams.length % 2)
-  console.log('compensate', compensate, teams.length * 2 - compensate)
-  console.log('schedule', schedule.length)
+//   console.log('teams.length', mock.teams.length)
+//   console.log('rest', mock.teams.length % 2)
+//   console.log('compensate', compensate, mock.teams.length * 2 - compensate)
+//   console.log('schedule', mock.schedule.length)
   
-  schedule.forEach((round, key) => {
-    console.log('============')
-    console.log('Round', key)
-    round.forEach(match => 
-      console.log(match[0] ? match[0].name : null, 'vs', match[1] ? match[1].name : null))
-  })
+//   mock.schedule.forEach((round, key) => {
+//     console.log('============')
+//     console.log('Round', key)
+//     round.forEach(match => 
+//       console.log(match[0] ? match[0].name : null, 'vs', match[1] ? match[1].name : null))
+//   })
   
-  t.equal(schedule.length, teams.length * 2 - compensate, 'the schedule should have a correct number of rounds')
-  t.ok(Object.keys(homeAwayCount).length, 'should have home-away count')
+  t.equal(mock.schedule.length, mock.teams.length * 2 - compensate, 'the schedule should have a correct number of rounds')
+  t.ok(Object.keys(mock.homeAwayCount).length, 'should have home-away count')
   
   // Check that each team has the same amount of home-away matches
-  teams.map(team => {
+  mock.teams.map(team => {
     t.equal(
-      homeAwayCount[team.name].home === homeAwayReferenceValues.home && homeAwayCount[team.name].away === homeAwayReferenceValues.away,
+      mock.homeAwayCount[team.name].home === mock.homeAwayReferenceValues.home &&
+      mock.homeAwayCount[team.name].away === mock.homeAwayReferenceValues.away,
       true,
       team.name + ': number of home-away matches')
   })
