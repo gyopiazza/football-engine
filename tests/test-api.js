164845756c4107c7914f13bb288cb2968527c6b9
Glitch (football-realmdb) <none>
2020-11-29 11:04:40 +0000

    💺🍙 Checkpoint ./tests/test-api.js:1334717/3574

--- tests/test-api.js
+++ tests/test-api.js
@@ -52,29 +52,28 @@ const mockSchedule = (teamsNum = 10, options = {}) => {
 
 test('API Tests', function (t) {
   
-  const mock = mockSchedule(20, { twolegs: true, shuffle: true })
+  const mock = mockSchedule(3, { twolegs: true, shuffle: true })
   const compensate = mock.teams.length % 2 ? 0 : 2
     
   t.equal(mock.schedule.length, mock.teams.length * 2 - compensate, 'the schedule should have a correct number of rounds')
   t.ok(Object.keys(mock.homeAwayCount).length, 'should have home-away count')
   
   // Check that each team has the same amount of home-away matches
-  mock.teams.map(team => {
-    t.equal(
-      mock.homeAwayCount[team.name].home === mock.homeAwayReferenceValues.home &&
-      mock.homeAwayCount[team.name].away === mock.homeAwayReferenceValues.away,
-      true,
-      team.name + ': number of home-away matches')
-  })
-  mock.teams.reduce(team => {
+  // mock.teams.map(team => {
+  //   t.equal(
+  //     mock.homeAwayCount[team.name].home === mock.homeAwayReferenceValues.home &&
+  //     mock.homeAwayCount[team.name].away === mock.homeAwayReferenceValues.away,
+  //     true,
+  //     team.name + ': number of home-away matches')
+  // })
+  
+  const ham = mock.teams.reduce((acc, team) => {
     return mock.homeAwayCount[team.name].home === mock.homeAwayReferenceValues.home &&
            mock.homeAwayCount[team.name].away === mock.homeAwayReferenceValues.away
-      ? 0
-      : 1
+      ? acc + 0
+      : acc + 1
   }, 0)
-  
-  
-  // console.log(mock.matches)
+  t.equal(ham, 0, 'Correct number of home-away matches')
   
   const standings = mock.matches
       .reduce(api.standingsReducer, [])
@@ -86,8 +85,53 @@ test('API Tests', function (t) {
     { id: 1, name: 'Team 1' },
     { id: 2, name: 'Team 2' },
     { id: 3, name: 'Team 3' },
+    { id: 4, name: 'Team 4' },
   ]
-  // console.log(standings)
+  
+  const matches = [
+    {
+      team_home: { id: 2, name: "Team 2" },
+      team_away: { id: 1, name: "Team 1" },
+      goals_home: 2,
+      goals_away: 1
+    },
+    {
+      team_home: { id: 3, name: "Team 3" },
+      team_away: { id: 4, name: "Team 4" },
+      goals_home: 1,
+      goals_away: 1
+    },
+    {
+      team_home: { id: 2, name: "Team 2" },
+      team_away: { id: 3, name: "Team 3" },
+      goals_home: 1,
+      goals_away: 2
+    },
+    {
+      team_home: { id: 1, name: "Team 1" },
+      team_away: { id: 4, name: "Team 4" },
+      goals_home: 2,
+      goals_away: 1
+    },
+    // {
+    //   team_home: { id: 4, name: "Team 4" },
+    //   team_away: { id: 2, name: "Team 2" },
+    //   goals_home: 1,
+    //   goals_away: 2
+    // },
+    // {
+    //   team_home: { id: 3, name: "Team 3" },
+    //   team_away: { id: 1, name: "Team 1" },
+    //   goals_home: 2,
+    //   goals_away: 1
+    // }
+  ];
+  
+  const standings2 = matches
+      .reduce(api.standingsReducer, [])
+      .sort(api.standingsSorter)
+  
+  console.log(standings2)
   
   t.end()
 })
